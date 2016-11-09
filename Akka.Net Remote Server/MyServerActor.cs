f04946b0ab0989d9d27a444ac50a76ff360256a4
PowerMogli <albix@gmx.net>
2016-11-09 16:14:35 +0100

    initial commit

---
+++ Akka.Net Remote Server/MyServerActor.cs
@@ -0,0 +1,30 @@
+﻿#region using directives
+
+using Akka.Actor;
+
+using Naip.CommunicationMessages.Messages;
+
+#endregion
+
+namespace Akka.Net_Remote_Server
+{
+    public class MyServerActor : ReceiveActor
+    {
+        #region Construction
+
+        public MyServerActor()
+        {
+            Receive<ReliableDeliveryEnvelope<ChangePrescriptionStatusToIssued>>(msg =>
+            {
+                Sender.Tell(new ReliableDeliveryAck(msg.MessageId));
+            });
+
+            Receive<ReliableDeliveryEnvelope<ChangePrescriptionStatusToNotIssued>>(msg =>
+            {
+                Sender.Tell(new ReliableDeliveryAck(msg.MessageId));
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ Akka.Net Remote Server/Program.cs
@@ -0,0 +1,53 @@
+﻿#region using directives
+
+using System;
+
+using Akka.Actor;
+using Akka.Configuration;
+
+#endregion
+
+namespace Akka.Net_Remote_Server
+{
+    class Program
+    {
+        #region Private Methods
+
+        private static void Main()
+        {
+            Config config = ConfigurationFactory.ParseString(@"
+            akka {  
+                actor {
+                    provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                    serializers {
+                        wire = ""Akka.Serialization.WireSerializer, Akka.Serialization.Wire""
+                    }
+                    serialization-bindings {
+                        ""System.Object"" = wire
+                    }
+                }
+                remote {
+                    helios.tcp {
+                        port = 8989
+                        hostname = FUEPC-0091.naip.de
+                        enable-ssl = true
+                    }
+                    helios.ssl {
+                        enable-ssl = true
+                        security {
+                            protocol = ""TLSv1.2""
+                        }                        
+                    }
+                }
+            }");
+
+            ActorSystem actorSystem = ActorSystem.Create("MyServer", config);
+
+            actorSystem.ActorOf<MyServerActor>("MyServerActor");
+
+            Console.ReadLine();
+        }
+
+        #endregion
+    }
+}
